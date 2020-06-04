5d07547fdc4a69abf41c5b5038f3036ced1bb3e3
nurulalisa <48412763+nurulalisa@users.noreply.github.com>
2020-06-05 05:47:02 +0800

    Create Home.js

---
+++ Insurance/src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Home = () => (
+  <div>
+    <h2>About Us</h2>
+    <br></br>
+    <p>An insurance company that bloom like lotus under the mud.</p>
+    <br></br>
+    <p>Company's policy to make sure all customers are all well taken care & satisfied with our sevices 💗</p>
+    <br></br>
+    <p>We listen to your story, we understand your concern.</p>
+    <br></br>
+    <p></p>
+    <br></br>
+
+    </div>
+)
