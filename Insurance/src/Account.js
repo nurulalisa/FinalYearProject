a0c16e8e323fd6cc4e9d76ede11adba162cc79fb
nurulalisa <48412763+nurulalisa@users.noreply.github.com>
2020-06-05 05:47:53 +0800

    Create Account.js

---
+++ Insurance/src/Account.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button} from 'react-bootstrap';
+
+export const Account = () => (
+   <div id = "container"> 
+   <h2>Fill In The Form</h2>
+  <script type = " text/javascript"></script>
+  <br/>
+  <input id = "full name" type = "text" placeholder = "FULL NAME"/>
+  <br/>
+    <br/>
+    <input id = "ic number" type = "text" placeholder = "IC NUMBER"></input>
+    <br/>
+    <br/>
+    <input id = "member" type = "text" placeholder = "MEMBER"></input>
+    <br/>
+    <br/>
+    <button onclick="" id = "btn1">SUBMIT</button>
+
+    </div>
+)
