c3c998c0ed8be857a4092fd462ddb0a6050bbd8f
nurulalisa <48412763+nurulalisa@users.noreply.github.com>
2020-06-05 05:44:54 +0800

    Create Layout.js

---
+++ Insurance/src/components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export const Layout = (props) => (
+  <Container>
+    {props.children}
+  </Container>
+)
+
+** props.childre allows anything to be executed
