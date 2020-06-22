8090c66538b42a14873f60afa76dc00d6b383aed
nurulalisa <48412763+nurulalisa@users.noreply.github.com>
2020-06-22 14:11:37 +0800

    Create form.sol

---
+++ form.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+// SPDX-License-Identifier:UNLICENSED
+contract Contract {
+
+  struct Form {
+   uint icNumb;
+   string name;
+   uint member;
+  }
+  event FormRecorded (
+   uint icNumb,
+   string name,
+   uint member
+   );
+
+  mapping(uint => Form) public forms;
+
+  function newForm (
+      uint _icNumb,
+      string memory _name,
+      uint _member
+      ) public {
+
+    forms[_icNumb] = Form (_icNumb, _name, _member);
+    emit FormRecorded (_icNumb, _name, _member);
+  }
+
+}
